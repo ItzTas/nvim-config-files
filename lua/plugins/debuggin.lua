cf2b4cf0848a427378f7daa8f1d8aad5e887aab8
ItzTas <talessabini2@gmail.com>
2024-08-03 03:54:36 -0300

    updated

--- lua/plugins/debuggin.lua
+++ lua/plugins/debuggin.lua
@@ -1,1 +1,22 @@
-return {}
+return {
+	"mfussenegger/nvim-dap",
+	config = function()
+		local dap = require("dap")
+
+		vim.keymap.set("n", "<F12>", function()
+			dap.step_out()
+		end)
+
+		vim.keymap.set("n", "<leader>dc", function()
+			dap.continue()
+		end)
+
+		vim.keymap.set("n", "<Leader>dt", function()
+			dap.toggle_breakpoint()
+		end)
+
+		vim.keymap.set("n", "<Leader>ds", function()
+			dap.set_breakpoint()
+		end)
+	end,
+}
