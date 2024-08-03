178e1ffb097608d3b865761fa250d6679480cfd4
ItzTas <talessabini2@gmail.com>
2024-08-03 03:03:06 -0300

    configs updated

---
+++ lua/plugins/barbar.lua
@@ -0,0 +1,21 @@
+return {
+    "romgrk/barbar.nvim",
+    config = function()
+        local map = vim.api.nvim_set_keymap
+        local opts = { noremap = true, silent = true }
+
+        map("n", "<Leader>h", ":BufferPrevious<CR>", opts)
+
+        map("n", "<Leader>l", ":BufferNext<CR>", opts)
+
+        map("n", "<Leader>p1", ":BufferGoto 1<CR>", opts)
+        map("n", "<Leader>p2", ":BufferGoto 2<CR>", opts)
+        map("n", "<Leader>p3", ":BufferGoto 3<CR>", opts)
+        map("n", "<Leader>p4", ":BufferGoto 4<CR>", opts)
+        map("n", "<Leader>p5", ":BufferGoto 5<CR>", opts)
+        map("n", "<Leader>p6", ":BufferGoto 6<CR>", opts)
+        map("n", "<Leader>p7", ":BufferGoto 7<CR>", opts)
+        map("n", "<Leader>p8", ":BufferGoto 8<CR>", opts)
+        map("n", "<Leader>p9", ":BufferGoto 9<CR>", opts)
+    end,
+}
---
+++ lua/plugins/comment.lua
@@ -0,0 +1,9 @@
+return {
+	"numToStr/Comment.nvim",
+	opts = {
+		toggler = {
+			line = "<leader>gc",
+			block = "<leader>gb",
+		},
+	},
+}
