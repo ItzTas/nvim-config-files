e7e4ffc1a3c08621f07f315e6a3b4c8606c061c0
ItzTas <talessabini2@gmail.com>
2024-08-05 15:43:27 -0300

    u

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,5 +2,15 @@ return {
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		vim.keymap.set("n", "<leader>gd", vim.cmd.Gvdiffsplit)
+		vim.keymap.set("n", "<leader>ggl", vim.cmd.GlLog, { desc = "GlLog" })
+
+		vim.keymap.set("n", "<leader>gl", function()
+			vim.cmd("Git log")
+		end, { desc = "git log" })
+
+		vim.keymap.set("n", "<leader>gra", function()
+			vim.cmd('Git log --oneline --graph --all --decorate --stat --pretty=format:"%h %d %s %an %ar"')
+		end, { desc = "git graph" })
 	end,
 }
