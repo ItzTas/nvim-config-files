9fb471bfa1341d7c5d96a566a89090db0d660022
ItzTas <talessabini2@gmail.com>
2024-08-10 08:42:45 -0400

    upated

--- lua/itz/remaps.lua
+++ lua/itz/remaps.lua
@@ -23,12 +23,12 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
 
--- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessioniner<CR>")
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
