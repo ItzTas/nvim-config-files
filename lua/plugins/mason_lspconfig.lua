ca860e036f588aa4dfaebe43c198233905ccfbef
ItzTas <talessabini2@gmail.com>
2024-08-10 21:16:29 -0400

    updated

--- lua/plugins/mason_lspconfig.lua
+++ lua/plugins/mason_lspconfig.lua
@@ -1,8 +1,10 @@
 return {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-        require("mason-lspconfig").setup({
-            ensure_installed = { "lua_ls", "tsserver" }
-        })
-    end
+	"williamboman/mason-lspconfig.nvim",
+	after = "nvim-lspconfig",
+	config = function()
+		require("mason-lspconfig").setup({
+			ensure_installed = { "lua_ls", "tsserver" },
+			automatic_installation = true,
+		})
+	end,
 }
