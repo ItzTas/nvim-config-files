d8031b6f70e73ac1387c810c98340d90302d96e8
ItzTas <talessabini2@gmail.com>
2024-08-14 16:00:05 -0400

    updated

--- lua/plugins/doge.lua
+++ lua/plugins/doge.lua
@@ -1,10 +1,14 @@
 return {
-    "kkoomen/vim-doge",
-    config = function()
-        vim.g.doge_enable_mappings = 0
-        vim.keymap.set("n", "<Leader>dg", "<Plug>(doge-generate)")
+	"kkoomen/vim-doge",
+	config = function()
+		vim.g.doge_enable_mappings = 0
+		vim.keymap.set("n", "<Leader>dg", "<Plug>(doge-generate)")
 
-        vim.keymap.set("n", "<C-j>", "<Plug>(doge-comment-jump-forward)")
-        vim.keymap.set("n", "<C-k>", "<Plug>(doge-comment-jump-backward)")
-    end,
+		vim.keymap.set("n", "<TAB>", "<Plug>(doge-comment-jump-forward)")
+		vim.keymap.set("n", "<S-TAB>", "<Plug>(doge-comment-jump-backward)")
+		vim.keymap.set("i", "<TAB>", "<Plug>(doge-comment-jump-forward)")
+		vim.keymap.set("i", "<S-TAB>", "<Plug>(doge-comment-jump-backward)")
+		vim.keymap.set("x", "<TAB>", "<Plug>(doge-comment-jump-forward)")
+		vim.keymap.set("x", "<S-TAB>", "<Plug>(doge-comment-jump-backward)")
+	end,
 }
