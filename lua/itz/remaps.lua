832a723f0b3683ae7ab61c768f77af4e1660f4f5
ItzTas <talessabini2@gmail.com>
2024-08-14 22:45:41 -0400

    updated

--- lua/itz/remaps.lua
+++ lua/itz/remaps.lua
@@ -16,6 +16,7 @@ vim.keymap.set("n", "<C-z>", "<Nop>")
 
 vim.keymap.set("x", "<leader>h", ":s/^/")
 vim.keymap.set("x", "<leader>l", ":s/$/")
+vim.keymap.set("x", "<leader>s", ":s/\\%V.*\\%V/")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -34,7 +35,7 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-vim.keymap.set("n", "<leader>su", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
