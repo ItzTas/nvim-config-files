1ffd95221991d35be863586ddf7b672c33867e5f
ItzTas <talessabini2@gmail.com>
2024-08-20 22:17:40 -0400

    updated

--- lua/plugins/comment.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"numToStr/Comment.nvim",
-	opts = {
-		toggler = {
-			line = "<leader>cc",
-			block = "<leader>cb",
-		},
-	},
-}
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -26,24 +26,21 @@ return {
                 end)
 
                 -- Actions
-                vim.keymap.set("n", "<leader>ghs", gitsigns.stage_hunk)
-                vim.keymap.set("n", "<leader>ghr", gitsigns.reset_hunk)
-                vim.keymap.set("v", "<leader>ghs", function()
-                    gitsigns.stage_ghunk({ vim.fn.line("."), vim.fn.line("v") })
+                vim.keymap.set("v", "<leader>ph", function()
+                    gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
                 end)
-                vim.keymap.set("v", "<leader>ghr", function()
+                vim.keymap.set("v", "<leader>pr", function()
                     gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
                 end)
-                vim.keymap.set("n", "<leader>ghS", gitsigns.stage_buffer)
                 vim.keymap.set("n", "<leader>ghu", gitsigns.undo_stage_hunk)
-                vim.keymap.set("n", "<leader>ghR", gitsigns.reset_buffer)
-                vim.keymap.set("n", "<leader>ghp", gitsigns.preview_hunk)
-                vim.keymap.set("n", "<leader>ghb", function()
+                vim.keymap.set("n", "<leader>pR", gitsigns.reset_buffer)
+                vim.keymap.set("n", "<leader>pe", gitsigns.preview_hunk)
+                vim.keymap.set("n", "<leader>gb", function()
                     gitsigns.blame_line({ full = true })
                 end)
                 vim.keymap.set("n", "<leader>gtb", gitsigns.toggle_current_line_blame)
                 -- Text object
-                vim.keymap.set({ "o", "x" }, "<leader>g", ":<C-U>Gitsigns select_hunk<CR>")
+                vim.keymap.set({ "o", "x" }, "<leader>c", ":<C-U>Gitsigns select_hunk<CR>")
             end,
         })
     end,
