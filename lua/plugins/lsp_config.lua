76b84aad70216ee51afbc3fd5fb9d2454e590364
ItzTas <talessabini2@gmail.com>
2024-08-22 16:52:45 -0400

    updated

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -1,58 +1,58 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "VonHeikemen/lsp-zero.nvim",
-            dependencies = {
-                "neovim/nvim-lspconfig", -- Configuração de LSP
-                "williamboman/mason.nvim", -- Gerenciador de servidores LSP
-                "williamboman/mason-lspconfig.nvim", -- Integração com lspconfig
-                "hrsh7th/nvim-cmp",      -- Autocompletar
-                "hrsh7th/cmp-nvim-lsp",  -- Autocompletar para LSP
-                "L3MON4D3/LuaSnip",      -- Snippets
-            },
-        },
-        config = function()
-            local lsp_zero = require("lsp-zero")
-            local cmp = require("cmp")
-            lsp_zero.on_attach(function(client, bufnr)
-                local opts = { buffer = bufnr, remap = false, desc = "LSP" }
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"VonHeikemen/lsp-zero.nvim",
+			dependencies = {
+				"neovim/nvim-lspconfig", -- Configuração de LSP
+				"williamboman/mason.nvim", -- Gerenciador de servidores LSP
+				"williamboman/mason-lspconfig.nvim", -- Integração com lspconfig
+				"hrsh7th/nvim-cmp", -- Autocompletar
+				"hrsh7th/cmp-nvim-lsp", -- Autocompletar para LSP
+				"L3MON4D3/LuaSnip", -- Snippets
+			},
+		},
+		config = function()
+			local lsp_zero = require("lsp-zero")
+			local cmp = require("cmp")
+			lsp_zero.on_attach(function(client, bufnr)
+				local opts = { buffer = bufnr, remap = false, desc = "LSP" }
 
-                vim.keymap.set("n", "gd", function()
-                    vim.lsp.buf.definition()
-                end, opts)
+				vim.keymap.set("n", "gd", function()
+					vim.lsp.buf.definition()
+				end, opts)
 
-                vim.keymap.set("n", "K", function()
-                    vim.lsp.buf.hover()
-                end, opts)
+				vim.keymap.set("n", "K", function()
+					vim.lsp.buf.hover()
+				end, opts)
 
-                vim.keymap.set("n", "<leader>vws", function()
-                    vim.lsp.buf.workspace_symbol()
-                end, opts)
+				vim.keymap.set("n", "<leader>vws", function()
+					vim.lsp.buf.workspace_symbol()
+				end, opts)
 
-                vim.keymap.set("n", "<leader>vd", function()
-                    vim.diagnostic.open_float()
-                end, opts)
+				vim.keymap.set("n", "<leader>vd", function()
+					vim.diagnostic.open_float()
+				end, opts)
 
-                vim.keymap.set("n", "[d", function()
-                    vim.diagnostic.goto_next()
-                end, opts)
+				vim.keymap.set("n", "[d", function()
+					vim.diagnostic.goto_next()
+				end, opts)
 
-                vim.keymap.set("n", "]d", function()
-                    vim.diagnostic.goto_prev()
-                end, opts)
+				vim.keymap.set("n", "]d", function()
+					vim.diagnostic.goto_prev()
+				end, opts)
 
-                vim.keymap.set("n", "<leader>ca", function()
-                    vim.lsp.buf.code_action()
-                end, opts)
+				vim.keymap.set("n", "<leader>ca", function()
+					vim.lsp.buf.code_action()
+				end, opts)
 
-                vim.keymap.set("n", "<leader>rf", function()
-                    vim.lsp.buf.references()
-                end, opts)
+				vim.keymap.set("n", "<leader>rf", function()
+					vim.lsp.buf.references()
+				end, opts)
 
-                vim.keymap.set("n", "<leader>rn", function()
-                    vim.lsp.buf.rename()
-                end, opts)
+				vim.keymap.set("n", "<leader>rn", function()
+					vim.lsp.buf.rename()
+				end, opts)
 
 				vim.keymap.set("n", "<C-h>", function()
 					vim.lsp.buf.signature_help()
@@ -67,34 +67,34 @@ return {
 				["<S-Tab>"] = cmp.mapping.complete(),
 			})
 
-            cmp.setup({
-                mapping = cmp_mappings,
-            })
+			cmp.setup({
+				mapping = cmp_mappings,
+			})
 
-            local lspconfig = require("lspconfig")
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities.textDocument.completion.completionItem.snippetSupport = true
+			local lspconfig = require("lspconfig")
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-            lspconfig.lua_ls.setup({})
-            lspconfig.tsserver.setup({})
-            lspconfig.eslint.setup({})
-            lspconfig.gopls.setup({})
-            lspconfig.golangci_lint_ls.setup({})
-            lspconfig.marksman.setup({})
-            lspconfig.markdown_oxide.setup({})
-            lspconfig.bashls.setup({})
-            lspconfig.yamlls.setup({})
-            lspconfig.sqlls.setup({})
-            lspconfig.basedpyright.setup({})
-            lspconfig.pylsp.setup({})
+			lspconfig.lua_ls.setup({})
+			lspconfig.tsserver.setup({})
+			lspconfig.eslint.setup({})
+			lspconfig.gopls.setup({})
+			lspconfig.golangci_lint_ls.setup({})
+			lspconfig.marksman.setup({})
+			lspconfig.markdown_oxide.setup({})
+			lspconfig.bashls.setup({})
+			lspconfig.yamlls.setup({})
+			lspconfig.sqlls.setup({})
+			lspconfig.basedpyright.setup({})
+			lspconfig.pylsp.setup({})
 
-            lspconfig.html.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.cssls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.cssmodules_ls.setup({})
-        end,
-    },
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.cssmodules_ls.setup({})
+		end,
+	},
 }
