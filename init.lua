19088c340ef41a513c03d6a9eb8c829e5eb2bf66
ItzTas <talessabini2@gmail.com>
2024-08-24 03:07:24 -0400

    updated

--- init.lua
+++ init.lua
@@ -6,10 +6,12 @@ require("itz.remaps")
 vim.cmd("colorscheme rose-pine")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_create_user_command("W", "write", {})
+
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
     pattern = { "*.*" },
     callback = function()
-        require("gitsigns").toggle_current_line_blame()
+       require("gitsigns").toggle_current_line_blame()
     end,
 })
 
@@ -19,7 +21,7 @@ vim.api.nvim_create_autocmd("BufWritePost", {
     pattern = "*",
     callback = function()
         if vim.lsp.buf.format then
-         vim.lsp.buf.format()
+            vim.lsp.buf.format()
         end
     end,
 })
