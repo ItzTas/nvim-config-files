adfe714b389f273c1d4ea4535729514c23d6d070
ItzTas <talessabini2@gmail.com>
2024-09-12 13:29:01 -0400

    update

--- init.lua
+++ init.lua
@@ -8,8 +8,8 @@ vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	pattern = { "*.*" },
-	callback = function()
-		require("gitsigns").toggle_current_line_blame()
-	end,
+    pattern = { "*.*" },
+    callback = function()
+        require("gitsigns").toggle_current_line_blame()
+    end,
 })
