e2c07ded93052512e6d313f8c67a0461005c017f
ItzTas <talessabini2@gmail.com>
2024-09-23 19:29:42 -0400

    updated

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -4,4 +4,19 @@ return {
     ---@type Flash.Config
     opts = {},
     keys = {},
+    config = function()
+        require("flash").setup({
+            jump = {
+                search = {
+                    same_line = true,
+                },
+            },
+        })
+
+        vim.keymap.set("n", "<leader>q", function()
+            require("flash").jump({
+                pattern = vim.fn.expand("<cword>"),
+            })
+        end)
+    end,
 }
