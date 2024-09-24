9a5d3dcf7745fd220d9b20925e0f324763e39073
ItzTas <talessabini2@gmail.com>
2024-09-24 01:26:05 -0400

    updated

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,6 +1,35 @@
 return {
-	"tpope/vim-surround",
-	dependencies = {
-		"tpope/vim-repeat",
+	"echasnovski/mini.surround",
+	opts = {
+		-- Add custom surroundings to be used on top of builtin ones. For more
+		-- information with examples, see `:h MiniSurround.config`.
+		custom_surroundings = nil,
+
+		-- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+		highlight_duration = 500,
+
+		-- Module mappings. Use `''` (empty string) to disable one.
+		mappings = {
+			add = "ma", -- Add surrounding in Normal and Visual modes
+			delete = "md", -- Delete surrounding
+			replace = "cs", -- Replace surrounding
+		},
+
+		-- Number of lines within which surrounding is searched
+		n_lines = 20,
+
+		-- Whether to respect selection type:
+		-- - Place surroundings on separate lines in linewise mode.
+		-- - Place surroundings on each line in blockwise mode.
+		respect_selection_type = false,
+
+		-- How to search for surrounding (first inside current line, then inside
+		-- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+		-- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
+		-- see `:h MiniSurround.config`.
+		search_method = "cover",
+
+		-- Whether to disable showing non-error feedback
+		silent = false,
 	},
 }
