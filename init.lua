3e8307d4c3be43427d8cfe667ba74aec2a9fc4a4
ItzTas <talessabini2@gmail.com>
2024-10-23 18:26:28 -0400

    estilos melhorados

--- init.lua
+++ init.lua
@@ -4,9 +4,12 @@ require("config.lazy")
 require("itz")
 require("itz.set")
 require("itz.remaps")
-vim.cmd("colorscheme catppuccin")
+vim.cmd("colorscheme nightfly")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
     pattern = { "*.*" },
--- lua/plugins/my_styles.lua
+++ lua/plugins/my_styles.lua
@@ -1,5 +1,41 @@
 return {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = {} },
+	{
+		"shaunsingh/solarized.nvim",
+		name = "solarized",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"dracula/vim",
+		name = "dracula",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"NLKNguyen/papercolor-theme",
+		name = "PaperColor",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"bluz71/vim-nightfly-colors",
+		name = "nightfly",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"folke/tokyonight.nvim",
+		name = "tokyonight",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {},
+	},
 	{
 		"rose-pine/neovim",
 		name = "rose-pine",
