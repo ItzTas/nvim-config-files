63d8ef3cf5e8c59db4e4d96b5d281e2c30c80b87
ItzTas <talessabini2@gmail.com>
2024-10-23 12:47:34 -0400

    tema mudado

--- init.lua
+++ init.lua
@@ -4,25 +4,25 @@ require("config.lazy")
 require("itz")
 require("itz.set")
 require("itz.remaps")
-vim.cmd("colorscheme rose-pine")
+vim.cmd("colorscheme catppuccin")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	pattern = { "*.*" },
-	callback = function()
-		require("gitsigns").toggle_current_line_blame()
-	end,
+    pattern = { "*.*" },
+    callback = function()
+        require("gitsigns").toggle_current_line_blame()
+    end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "netrw",
-	callback = function()
-		local clients = vim.lsp.buf_get_clients(0)
-		if next(clients) ~= nil then
-			for _, client in pairs(clients) do
-				vim.lsp.buf_detach_client(0, client.id)
-			end
-		end
-	end,
+    pattern = "netrw",
+    callback = function()
+        local clients = vim.lsp.buf_get_clients(0)
+        if next(clients) ~= nil then
+            for _, client in pairs(clients) do
+                vim.lsp.buf_detach_client(0, client.id)
+            end
+        end
+    end,
 })
