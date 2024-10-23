007535e9819860b2549f009b5452baeb0b65513d
ItzTas <talessabini2@gmail.com>
2024-10-23 18:27:40 -0400

    mudado

--- init.lua
+++ init.lua
@@ -12,20 +12,20 @@ vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none" })
 vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-    pattern = { "*.*" },
-    callback = function()
-        require("gitsigns").toggle_current_line_blame()
-    end,
+	pattern = { "*.*" },
+	callback = function()
+		require("gitsigns").toggle_current_line_blame()
+	end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "netrw",
-    callback = function()
-        local clients = vim.lsp.buf_get_clients(0)
-        if next(clients) ~= nil then
-            for _, client in pairs(clients) do
-                vim.lsp.buf_detach_client(0, client.id)
-            end
-        end
-    end,
+	pattern = "netrw",
+	callback = function()
+		local clients = vim.lsp.buf_get_clients(0)
+		if next(clients) ~= nil then
+			for _, client in pairs(clients) do
+				vim.lsp.buf_detach_client(0, client.id)
+			end
+		end
+	end,
 })
