5166973b3fefe341a040cf28b056e0b0ca96e60f
ItzTas <talessabini2@gmail.com>
2024-10-25 20:35:12 -0400

    mudanças no lsp

--- lua/plugins/mason_lspconfig.lua
+++ lua/plugins/mason_lspconfig.lua
@@ -1,30 +1,29 @@
 return {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-        "williamboman/mason.nvim",
-    },
-    after = "nvim-lspconfig",
-    config = function()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "ts_ls",
-                "pylsp",
-                "eslint",
-                "lua_ls",
-                "gopls",
-                "golangci_lint_ls",
-                "marksman",
-                "markdown_oxide",
-                "bashls",
-                "yamlls",
-                "sqlls",
-                "html",
-                "cssls",
-                "cssmodules_ls",
-                "docker_compose_language_service",
-                "dockerls",
-            },
-            automatic_installation = true,
-        })
-    end,
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+	},
+	after = "nvim-lspconfig",
+	config = function()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"ts_ls",
+				"pylsp",
+				"eslint",
+				"lua_ls",
+				"gopls",
+				"golangci_lint_ls",
+				"marksman",
+				"markdown_oxide",
+				"bashls",
+				"yamlls",
+				"html",
+				"cssls",
+				"cssmodules_ls",
+				"docker_compose_language_service",
+				"dockerls",
+			},
+			automatic_installation = true,
+		})
+	end,
 }
