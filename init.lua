6280f9642e6e931fe39cd2153c82479e74c772bb
ItzTas <talessabini2@gmail.com>
2024-10-27 01:47:09 -0400

    ia mudada

--- init.lua
+++ init.lua
@@ -19,20 +19,20 @@ vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 vim.api.nvim_set_hl(0, "WildMenu", { bg = "none" })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	pattern = { "*.*" },
-	callback = function()
-		require("gitsigns").toggle_current_line_blame()
-	end,
+    pattern = { "*.*" },
+    callback = function()
+        require("gitsigns").toggle_current_line_blame()
+    end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "netrw",
-	callback = function()
-		local clients = vim.lsp.buf_get_clients(0)
-		if next(clients) ~= nil then
-			for _, client in pairs(clients) do
-				vim.lsp.buf_detach_client(0, client.id)
-			end
-		end
-	end,
+    pattern = "netrw",
+    callback = function()
+        local clients = vim.lsp.buf_get_clients(0)
+        if next(clients) ~= nil then
+            for _, client in pairs(clients) do
+                vim.lsp.buf_detach_client(0, client.id)
+            end
+        end
+    end,
 })
--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,28 +1,18 @@
 return {
-    "codota/tabnine-nvim",
-    build = "./dl_binaries.sh",
-    config = function()
-        require("tabnine").setup({
-            disable_auto_comment = true,
-            accept_keymap = false,
-            dismiss_keymap = "<C-]>",
-            debounce_ms = 800,
-            suggestion_color = { gui = "#808080", cterm = 244 },
-            codelens_color = { gui = "#808080", cterm = 244 },
-            codelens_enabled = true,
-            exclude_filetypes = { "TelescopePrompt", "NvimTree" },
-            log_file_path = nil,
-            ignore_certificate_errors = false,
-        })
-
-        vim.keymap.set("i", "<C-b>", function()
-            if require("tabnine.keymaps").has_suggestion() then
-                return require("tabnine.keymaps").accept_suggestion()
-            elseif require("luasnip").jumpable(1) then
-                return require("luasnip").jump(1)
-            else
-                return "<C-b>"
-            end
-        end, { expr = true })
-    end,
+	"codota/tabnine-nvim",
+	build = "./dl_binaries.sh",
+	config = function()
+		require("tabnine").setup({
+			disable_auto_comment = true,
+			accept_keymap = "<C-b>",
+			dismiss_keymap = "<C-]>",
+			debounce_ms = 800,
+			suggestion_color = { gui = "#808080", cterm = 244 },
+			codelens_color = { gui = "#808080", cterm = 244 },
+			codelens_enabled = true,
+			exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+			log_file_path = nil,
+			ignore_certificate_errors = false,
+		})
+	end,
 }
