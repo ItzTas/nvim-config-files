35d356f05b81e0ddce396fff86e29bba4f70dcac
ItzTas <talessabini2@gmail.com>
2024-10-26 22:55:25 -0400

    new aditions to treesitter

--- lua/plugins/debuggin.lua
+++ lua/plugins/debuggin.lua
@@ -1,95 +1,95 @@
 return {
-    {
-        "nvim-neotest/neotest",
-        dependencies = {
-            "nvim-neotest/nvim-nio",
-            "nvim-lua/plenary.nvim",
-            "antoinemadec/FixCursorHold.nvim",
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-neotest/neotest-plenary",
-            "fredrikaverpil/neotest-golang",
-            "mfussenegger/nvim-dap",
-            "nvim-neotest/neotest-jest",
-        },
-        config = function()
-            require("neotest").setup({
-                adapters = {
-                    require("neotest-plenary"),
-                    require("neotest-golang"),
-                    require("neotest-jest")({
-                        jestCommand = "npm test --",
-                        env = { CI = true },
-                        cwd = function(path)
-                            return vim.fn.getcwd()
-                        end,
-                        jestConfigFile = function(file)
-                            if string.find(file, "/packages/") then
-                                return string.match(file, "(.-/[^/]+/)src") .. "jest.config.ts"
-                            end
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-neotest/neotest-plenary",
+			"fredrikaverpil/neotest-golang",
+			"mfussenegger/nvim-dap",
+			"nvim-neotest/neotest-jest",
+		},
+		config = function()
+			require("neotest").setup({
+				adapters = {
+					require("neotest-plenary"),
+					require("neotest-golang"),
+					require("neotest-jest")({
+						jestCommand = "npm test --",
+						env = { CI = true },
+						cwd = function(path)
+							return vim.fn.getcwd()
+						end,
+						jestConfigFile = function(file)
+							if string.find(file, "/packages/") then
+								return string.match(file, "(.-/[^/]+/)src") .. "jest.config.ts"
+							end
 
-                            return vim.fn.getcwd() .. "/jest.config.ts"
-                        end,
-                    }),
-                },
-            })
+							return vim.fn.getcwd() .. "/jest.config.ts"
+						end,
+					}),
+				},
+			})
 
-            vim.keymap.set("n", "<leader>dp", function()
-                require("neotest").run.run({ jestCommand = "jest --watch " })
-            end)
+			vim.keymap.set("n", "<leader>dp", function()
+				require("neotest").run.run({ jestCommand = "jest --watch " })
+			end)
 
-            vim.keymap.set("n", "<leader>dr", function()
-                require("neotest").run.run()
-            end)
+			vim.keymap.set("n", "<leader>dr", function()
+				require("neotest").run.run()
+			end)
 
-            vim.keymap.set("n", "<leader>da", function()
-                require("neotest").run.attach()
-            end)
+			vim.keymap.set("n", "<leader>da", function()
+				require("neotest").run.attach()
+			end)
 
-            vim.keymap.set("n", "<leader>df", function()
-                require("neotest").run.run(vim.fn.expand("%"))
-            end)
-        end,
-    },
-    {
-        "mfussenegger/nvim-dap",
-        dependencies = {
-            "nvim-neotest/nvim-nio",
-            "rcarriga/nvim-dap-ui",
-            "leoluz/nvim-dap-go",
-            "folke/lazydev.nvim",
-        },
-        config = function()
-            local dap = require("dap")
+			vim.keymap.set("n", "<leader>dR", function()
+				require("neotest").run.run(vim.fn.expand("%"))
+			end)
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"rcarriga/nvim-dap-ui",
+			"leoluz/nvim-dap-go",
+			"folke/lazydev.nvim",
+		},
+		config = function()
+			local dap = require("dap")
 
-            require("dap-go").setup({})
+			require("dap-go").setup({})
 
-            local dapui = require("dapui")
+			local dapui = require("dapui")
 
-            dapui.setup({})
+			dapui.setup({})
 
-            dap.listeners.before.attach.dapui_config = function()
-                dapui.open()
-            end
-            dap.listeners.before.launch.dapui_config = function()
-                dapui.open()
-            end
-            dap.listeners.before.event_terminated.dapui_config = function()
-                dapui.close()
-            end
-            dap.listeners.before.event_exited.dapui_config = function()
-                dapui.close()
-            end
-            vim.keymap.set("n", "<F12>", function()
-                dap.step_into()
-            end)
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+			vim.keymap.set("n", "<F12>", function()
+				dap.step_into()
+			end)
 
-            vim.keymap.set("n", "<leader>dc", function()
-                dap.continue()
-            end)
+			vim.keymap.set("n", "<leader>dc", function()
+				dap.continue()
+			end)
 
-            vim.keymap.set("n", "<Leader>dt", function()
-                dap.toggle_breakpoint()
-            end)
-        end,
-    },
+			vim.keymap.set("n", "<Leader>dt", function()
+				dap.toggle_breakpoint()
+			end)
+		end,
+	},
 }
--- lua/plugins/treesitter-textobjects.lua
+++ lua/plugins/treesitter-textobjects.lua
@@ -1,66 +1,74 @@
 return {
-	"nvim-treesitter/nvim-treesitter-textobjects",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			textobjects = {
-				select = {
-					enable = true,
-					keymaps = {
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
-						["aa"] = "@parameter.outer",
-						["ia"] = "@parameter.inner",
-						["ii"] = "@conditional.inner",
-						["ai"] = "@conditional.outer",
-						["ao"] = "@block.outer",
-						["io"] = "@block.inner",
-						["al"] = "@loop.outer",
-						["il"] = "@loop.inner",
-						["ae"] = "@assignment.lhs",
-						["ie"] = "@assignment.rhs",
-					},
-				},
-				move = {
-					enable = true,
-					set_jumps = true,
-					goto_next_start = {
-						["]m"] = "@function.outer",
-						["]c"] = "@class.outer",
-					},
-					goto_next_end = {
-						["]M"] = "@function.outer",
-						["]C"] = "@class.outer",
-					},
-					goto_previous_start = {
-						["[m"] = "@function.outer",
-						["[c"] = "@class.outer",
-					},
-					goto_previous_end = {
-						["[M"] = "@function.outer",
-						["[C"] = "@class.outer",
-					},
-				},
-				lsp_interop = {
-					enable = true,
-					border = "single",
-					floating_preview_opts = {},
-					peek_definition_code = {
-						["<leader>df"] = "@function.outer",
-						["<leader>dF"] = "@class.outer",
-					},
-				},
-				swap = {
-					enable = true,
-					swap_next = {
-						["<leader>mn"] = "@parameter.inner",
-					},
-					swap_previous = {
-						["<leader>ml"] = "@parameter.inner",
-					},
-				},
-			},
-		})
-	end,
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            textobjects = {
+                select = {
+                    enable = true,
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                        ["aa"] = "@parameter.outer",
+                        ["ia"] = "@parameter.inner",
+                        ["ii"] = "@conditional.inner",
+                        ["ai"] = "@conditional.outer",
+                        ["ao"] = "@block.outer",
+                        ["io"] = "@block.inner",
+                        ["al"] = "@loop.outer",
+                        ["il"] = "@loop.inner",
+                        ["ae"] = "@assignment.lhs",
+                        ["ie"] = "@assignment.rhs",
+                        ["aq"] = "@assignment.outer",
+                        ["iq"] = "@assignment.inner",
+                        ["ar"] = "@return.outer",
+                        ["ir"] = "@return.inner",
+                        ["in"] = "@number.inner",
+                        ["iv"] = "@statement.outer",
+                        ["am"] = "@comment.outer",
+                        ["im"] = "@comment.inner",
+                    },
+                },
+                move = {
+                    enable = true,
+                    set_jumps = true,
+                    goto_next_start = {
+                        ["]m"] = "@function.outer",
+                        ["]c"] = "@class.outer",
+                    },
+                    goto_next_end = {
+                        ["]M"] = "@function.outer",
+                        ["]C"] = "@class.outer",
+                    },
+                    goto_previous_start = {
+                        ["[m"] = "@function.outer",
+                        ["[c"] = "@class.outer",
+                    },
+                    goto_previous_end = {
+                        ["[M"] = "@function.outer",
+                        ["[C"] = "@class.outer",
+                    },
+                },
+                lsp_interop = {
+                    enable = true,
+                    border = "single",
+                    floating_preview_opts = {},
+                    peek_definition_code = {
+                        ["<leader>df"] = "@function.outer",
+                        ["<leader>dF"] = "@class.outer",
+                    },
+                },
+                swap = {
+                    enable = true,
+                    swap_next = {
+                        ["mn"] = "@parameter.inner",
+                    },
+                    swap_previous = {
+                        ["mp"] = "@parameter.inner",
+                    },
+                },
+            },
+        })
+    end,
 }
