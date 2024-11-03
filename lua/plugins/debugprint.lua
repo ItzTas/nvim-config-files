a902378356818f6145943e1b8105bbacbea590f5
ItzTas <talessabini2@gmail.com>
2024-11-02 21:13:19 -0400

    better documentation

--- lua/plugins/debugprint.lua
+++ lua/plugins/debugprint.lua
@@ -1,6 +1,34 @@
 return {
-    "andrewferrier/debugprint.nvim",
-    config = function()
-        require("debugprint").setup()
-    end,
+	"andrewferrier/debugprint.nvim",
+	dependencies = {
+		"echasnovski/mini.nvim",
+	},
+	opts = {
+		keymaps = {
+			normal = {
+				plain_below = "g?p",
+				plain_above = "g?P",
+				variable_below = "g?v",
+				variable_above = "g?V",
+				variable_below_alwaysprompt = nil,
+				variable_above_alwaysprompt = nil,
+				textobj_below = "g?o",
+				textobj_above = "g?O",
+				toggle_comment_debug_prints = nil,
+				delete_debug_prints = nil,
+			},
+			visual = {
+				variable_below = "g?v",
+				variable_above = "g?V",
+			},
+		},
+		commands = {
+			toggle_comment_debug_prints = "ToggleCommentDebugPrints",
+			delete_debug_prints = "DeleteDebugPrints",
+		},
+		-- … Other options
+	},
+	config = function()
+		require("debugprint").setup()
+	end,
 }
