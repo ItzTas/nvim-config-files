fa222c97e5d491cd293a6642d9fe8089c4e76bfc
ItzTas <talessabini2@gmail.com>
2024-11-18 13:09:55 -0400

    java lsp

--- lua/itz/remaps.lua
+++ lua/itz/remaps.lua
@@ -9,49 +9,49 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 do
-    local function t()
-        local trouble = require("trouble")
+	local function t()
+		local trouble = require("trouble")
 
-        vim.keymap.set("n", "<C-j>", function()
-            if trouble.is_open() then
-                trouble.next()
-                trouble.jump()
-            else
-                vim.diagnostic.goto_next()
-            end
-        end)
+		vim.keymap.set("n", "<C-j>", function()
+			if trouble.is_open() then
+				trouble.next()
+				trouble.jump()
+			else
+				vim.diagnostic.goto_next()
+			end
+		end)
 
-        vim.keymap.set("n", "<C-k>", function()
-            if trouble.is_open() then
-                trouble.prev()
-                trouble.jump()
-            else
-                vim.diagnostic.goto_prev()
-            end
-        end)
-    end
+		vim.keymap.set("n", "<C-k>", function()
+			if trouble.is_open() then
+				trouble.prev()
+				trouble.jump()
+			else
+				vim.diagnostic.goto_prev()
+			end
+		end)
+	end
 
-    t()
+	t()
 end
 
 vim.keymap.set("n", "<leader>o", function()
-    local count = vim.v.count1
-    for _ = 1, count do
-        vim.cmd("normal! mz")
-        vim.cmd("normal! o")
-        vim.cmd("normal! `z")
-        vim.cmd("delmarks z")
-    end
+	local count = vim.v.count1
+	for _ = 1, count do
+		vim.cmd("normal! mz")
+		vim.cmd("normal! o")
+		vim.cmd("normal! `z")
+		vim.cmd("delmarks z")
+	end
 end)
 
 vim.keymap.set("n", "<leader>O", function()
-    local count = vim.v.count1
-    for _ = 1, count do
-        vim.cmd("normal! mz")
-        vim.cmd("normal! O")
-        vim.cmd("normal! `z")
-        vim.cmd("delmarks z")
-    end
+	local count = vim.v.count1
+	for _ = 1, count do
+		vim.cmd("normal! mz")
+		vim.cmd("normal! O")
+		vim.cmd("normal! `z")
+		vim.cmd("delmarks z")
+	end
 end)
 
 vim.keymap.set("n", "<leader>-", "<C-w>s")
@@ -61,6 +61,9 @@ vim.keymap.set("n", "<leader><", "<C-w><")
 vim.keymap.set("n", "<leader>>", "<C-w>>")
 
 vim.keymap.set({ "n", "v", "x", "o", "i" }, "<C-c>", "<Esc>")
+vim.keymap.set("i", "ĉ", "<Esc>")
+
+vim.keymap.set("n", "<CR>", "<Nop>")
 
 vim.keymap.set("n", "<C-z>", "<Nop>")
 
@@ -91,25 +94,25 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 local function escape_pattern(str)
-    return str:gsub("([%.%-%+%*%?%[%]%^%$%(%)])", "%%%1")
+	return str:gsub("([%.%-%+%*%?%[%]%^%$%(%)])", "%%%1")
 end
 
 vim.g.escaped_word = ""
 
 vim.keymap.set("n", "<leader>mr", function()
-    local word = vim.fn.input("Grep > ")
-    vim.g.escaped_word = escape_pattern(word)
-    vim.cmd("vimgrep /" .. vim.g.escaped_word .. "/gn ./**")
-    vim.cmd("copen")
+	local word = vim.fn.input("Grep > ")
+	vim.g.escaped_word = escape_pattern(word)
+	vim.cmd("vimgrep /" .. vim.g.escaped_word .. "/gn ./**")
+	vim.cmd("copen")
 end)
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "qf",
-    callback = function()
-        vim.keymap.set("n", "<C-j>", ":cnext<CR><C-w>j", { buffer = 0 })
-        vim.keymap.set("n", "<C-k>", ":cprev<CR><C-w>j", { buffer = 0 })
-        vim.keymap.set("n", "<C-r>", ":cdo %s/" .. vim.g.escaped_word .. "//gc<left><left><left>", { buffer = 0 })
-    end,
+	pattern = "qf",
+	callback = function()
+		vim.keymap.set("n", "<C-j>", ":cnext<CR><C-w>j", { buffer = 0 })
+		vim.keymap.set("n", "<C-k>", ":cprev<CR><C-w>j", { buffer = 0 })
+		vim.keymap.set("n", "<C-r>", ":cdo %s/" .. vim.g.escaped_word .. "//gc<left><left><left>", { buffer = 0 })
+	end,
 })
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
@@ -125,25 +128,25 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>S", 'yiW:%s/<C-r>"/<C-r>"/gI<Left><Left><Left>')
 
 vim.keymap.set("n", "V", function()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("V", true, false, true), "n", true)
-    vim.api.nvim_set_hl(0, "Visual", { bg = "#5e81ac", fg = "#ffffff" })
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("V", true, false, true), "n", true)
+	vim.api.nvim_set_hl(0, "Visual", { bg = "#5e81ac", fg = "#ffffff" })
 end)
 
 vim.keymap.set("n", "v", function()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("v", true, false, true), "n", true)
-    vim.api.nvim_set_hl(0, "Visual", { bg = "#5e81ac", fg = "#ffffff" })
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("v", true, false, true), "n", true)
+	vim.api.nvim_set_hl(0, "Visual", { bg = "#5e81ac", fg = "#ffffff" })
 end)
 
 vim.keymap.set("v", "<C-p>", function()
-    vim.api.nvim_set_hl(0, "Visual", { bg = "#5e81ac", fg = "#ffffff" })
+	vim.api.nvim_set_hl(0, "Visual", { bg = "#5e81ac", fg = "#ffffff" })
 end)
 
 vim.keymap.set("n", "<C-v>", function()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-v>", true, false, true), "n", true)
-    vim.api.nvim_set_hl(0, "Visual", { bg = "#5e81ac", fg = "#ffffff" })
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-v>", true, false, true), "n", true)
+	vim.api.nvim_set_hl(0, "Visual", { bg = "#5e81ac", fg = "#ffffff" })
 end)
 
 vim.keymap.set("n", "<C-q>", function()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-v>", true, false, true), "n", true)
-    vim.api.nvim_set_hl(0, "Visual", { bg = "#5e81ac", fg = "#ffffff" })
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-v>", true, false, true), "n", true)
+	vim.api.nvim_set_hl(0, "Visual", { bg = "#5e81ac", fg = "#ffffff" })
 end)
