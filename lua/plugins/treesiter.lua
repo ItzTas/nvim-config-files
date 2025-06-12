c15165c208bf26101ce9ef65d5021a0dfff888c4
Tales <59647566+ItzTas@users.noreply.github.com>
2025-06-11 20:10:03 -0400

    treesitter changes

--- lua/plugins/treesiter.lua
+++ lua/plugins/treesiter.lua
@@ -1,30 +1,27 @@
-local M = {
-	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true })()
-	end,
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			auto_install = true,
-			ensure_installed = {
-				"python",
-				"javascript",
-				"go",
-				"c",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"markdown",
-				"markdown_inline",
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    lazy = false,
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            auto_install = true,
+            ensure_installed = {
+                "python",
+                "javascript",
+                "go",
+                "c",
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                "markdown",
+                "markdown_inline",
                 "json",
-			},
-			highlight = {
-				enable = true,
-			},
-			indent = { enable = true },
-		})
-	end,
+            },
+            highlight = {
+                enable = true,
+            },
+            indent = { enable = true },
+        })
+    end,
 }
-
-return { M }
