dc9e489f85f8a9ad6614409c18101550d65d239d
Tales <59647566+ItzTas@users.noreply.github.com>
2025-06-11 20:23:48 -0400

    mason changes

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,53 @@
 return {
-	"williamboman/mason.nvim",
-	opts = {},
-	config = function()
-		require("mason").setup({})
-	end,
+    {
+        "williamboman/mason.nvim",
+        opts = {},
+        config = function()
+            require("mason").setup({})
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+        },
+        after = "nvim-lspconfig",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "ts_ls",
+                    "pylsp",
+                    "eslint",
+                    "lua_ls",
+                    "gopls",
+                    "golangci_lint_ls",
+                    "marksman",
+                    "markdown_oxide",
+                    "hyprls",
+                    "bashls",
+                    "yamlls",
+                    "html",
+                    "cssls",
+                    "cssmodules_ls",
+                    "docker_compose_language_service",
+                    "dockerls",
+                },
+                automatic_installation = true,
+            })
+        end,
+    },
+    {
+        "jay-babu/mason-null-ls.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "nvimtools/none-ls.nvim",
+        },
+        config = function()
+            require("plugins.null-ls")
+
+            require("mason-null-ls").setup({
+                automatic_installation = true,
+            })
+        end,
+    },
 }
