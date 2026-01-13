1c30d51b54d11c802dfe97f32cec18d9cc18f3b4
Tales <59647566+ItzTas@users.noreply.github.com>
2026-01-13 16:34:29 -0400

    chore: remaps & null ls

--- lua/itz/remaps.lua
+++ lua/itz/remaps.lua
@@ -3,6 +3,9 @@ vim.keymap.set("n", "<leader>f", _G.format_file)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("i", "<C-BS>", "<C-w>")
+vim.keymap.set("i", "<C-h>", "<C-w>")
+
 do
     local function t()
         local trouble = require("trouble")
--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -6,12 +6,13 @@ return {
         "nvim-treesitter/nvim-treesitter",
     },
     config = function()
-        require("go").setup()
+        require("go").setup({
+            tag_options = "",
+        })
 
         vim.keymap.set("n", "<leader>gt", vim.cmd.GoAddTag)
     end,
     event = { "CmdlineEnter" },
     ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    build = ':lua require("go.install").update_all_sync()',
 }
-
