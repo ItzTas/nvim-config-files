590cb5b8b96bf230d152d4dba31a833f9442acc2
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-12 11:19:58 -0400

    json options

--- lua/itz/set.lua
+++ lua/itz/set.lua
@@ -42,3 +42,12 @@ vim.opt.wrap = true
 
 vim.g.python3_host_prog = vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
 vim.g.molten_kernel = "python3"
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "json",
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.softtabstop = 2
+    end,
+})
