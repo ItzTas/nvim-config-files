b18ee4c7efc2fd0827c0f53d4b0fa93c3413c7a4
Tales <59647566+ItzTas@users.noreply.github.com>
2026-03-27 18:34:21 -0400

    name changed

--- lua/commands/init.lua
+++ lua/commands/init.lua
@@ -2,6 +2,6 @@ local path = debug.getinfo(1, "S").source:sub(2):match("(.*/)")
 for _, file in ipairs(vim.fn.glob(path .. "*.lua", false, true)) do
     local module = file:match(".*/(.*)%.lua$")
     if module ~= "init" then
-        require("functions." .. module)
+        require("commands." .. module)
     end
 end
