9976e99c629c7c7f3f4c09eb71019c31fcdde288
Tales <59647566+ItzTas@users.noreply.github.com>
2026-03-27 22:06:33 -0400

    new utils

---
+++ lua/plugins/utils/open-github-url.lua
@@ -0,0 +1,11 @@
+return {
+    "tetzng/open-github-url.nvim",
+
+    keys = {
+        {
+            "<leader>gh",
+            "<cmd>OpenGitHubUrlUnderCursor<cr>",
+            desc = "Open GitHub URL under cursor",
+        },
+    },
+}
