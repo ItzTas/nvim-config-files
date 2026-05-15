2fa5b70f8ab1e8dbed735ba7ac23092a4b094b5e
Tales <tales.sabini@pigz.com.br>
2026-05-15 05:24:06 -0400

    refactoring disabled

--- lua/plugins/editing/refactoring.lua
+++ lua/plugins/editing/refactoring.lua
@@ -4,6 +4,7 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
 	},
+    enabled = false,
 	config = function()
 		require("refactoring").setup({
 			prompt_func_return_type = {
